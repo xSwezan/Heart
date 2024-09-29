d3e805245d980d5e772b524c0a1270bbeb2dca3a
xSwezan <101433236+xSwezan@users.noreply.github.com>
2024-09-29 18:58:20 +0200

    v0.1.0

---
+++ init.lua
@@ -0,0 +1,28 @@
+--[[
+
+ /$$   /$$                                 /$$
+| $$  | $$                                | $$
+| $$  | $$  /$$$$$$   /$$$$$$   /$$$$$$  /$$$$$$
+| $$$$$$$$ /$$__  $$ |____  $$ /$$__  $$|_  $$_/
+| $$__  $$| $$$$$$$$  /$$$$$$$| $$  \__/  | $$
+| $$  | $$| $$_____/ /$$__  $$| $$        | $$ /$$
+| $$  | $$|  $$$$$$$|  $$$$$$$| $$        |  $$$$/
+|__/  |__/ \_______/ \_______/|__/         \___/
+
+--]]
+
+local BASE = (...).."."
+
+require(BASE.."classes.Vector2")
+require(BASE.."classes.Sprite")
+require(BASE.."classes.Spring")
+require(BASE.."classes.Sound")
+require(BASE.."classes.Color")
+require(BASE.."classes.Task")
+
+Vector2 = Vector2
+Sprite = Sprite
+Spring = Spring
+Sound = Sound
+Color = Color
+Task = Task
